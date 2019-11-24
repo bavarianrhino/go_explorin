444cb44722ab8d060a3ad958a29ddf014d0e3394
bavarianrhino <rjriesenberger@gmail.com>
2019-11-24 17:48:39 -0500

    changed title and headline

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,25 +2,19 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import React from 'react';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>GoExplorin</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+                <p>The world is your oyster....but more importantly it's a garden...dig it.</p>
+                <p>If you get lost, the{' '}<a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">docs</a>{' '}will be your guide.</p>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
