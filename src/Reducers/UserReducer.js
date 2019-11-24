1f459aae50f3267fc89ec06cbc7b1f6f55fe6299
bavarianrhino <rjriesenberger@gmail.com>
2019-11-24 17:38:30 -0500

    added store and devtool to top level of app

---
+++ src/Reducers/UserReducer.js
@@ -0,0 +1,16 @@
+export const UserReducer = (
+
+	state = {
+        loading: false,
+        currentUser: null,
+        longitude: null,
+        latitude: null
+        
+	}, action) => {
+
+        switch (action.type) {
+
+			default:
+				return state;
+		}
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import 'semantic-ui-css/semantic.min.css'
+
+import { Provider } from 'react-redux';
+import { RootReducer } from './Reducers/RootReducer';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+
+const store = createStore(RootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
